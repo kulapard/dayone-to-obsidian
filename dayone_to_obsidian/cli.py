f6e27f4d1fbaa44f95d985bdec225afbe2e80426
Taras Drapalyuk <taras@drapalyuk.com>
2023-12-08 14:31:28 +0400

    FIx default --target

--- dayone_to_obsidian/cli.py
+++ dayone_to_obsidian/cli.py
@@ -46,7 +46,8 @@ def version() -> None:
     "--tag-prefix",
     "tag_prefix",
     type=str,
-    help="Prefix for tags. By default it's empty. Example: `--tag-prefix=dayone:` will convert `tag` to `dayone:tag`.",
+    help="Prefix for tags. By default it's empty. "
+    "Example: `--tag-prefix=dayone:` will convert `tag` to `dayone:tag`.",
     default=DEFAULT_OPTIONS.tag_prefix,
 )
 def run(json: Path, target_dir: Path, force: bool, tag_prefix: str) -> None:
